0283b7462ad725a25a1c94909def65a3225302ae
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-11-10 21:35:08 -0800

    initial types from openapi schema

---
+++ src/types.rs
@@ -0,0 +1,325 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+pub struct Engine {
+    pub id: String,
+    pub object: String,
+    pub created: u32, // nullable: true
+    pub ready: bool,
+}
+
+pub struct Model {
+    pub id: String,
+    pub object: String,
+    pub created: u32,
+    pub owned_by: String,
+}
+
+pub struct OpenAIFile {
+    pub id: String,
+    pub object: String,
+    pub bytes: u32,
+    pub created_at: u32,
+    pub filename: String,
+    pub purpose: String,
+    pub status: Option<String>,
+    pub status_details: Option<serde_json::Value>, // nullable: true
+}
+
+pub struct FineTune {
+    pub id: String,
+    pub object: String,
+    pub created_at: u32,
+    pub updated_at: u32,
+    pub model: String,
+    pub fine_tuned_model: String, // nullable: true
+    pub organization_id: String,
+    pub status: String,
+    pub hyperparams: serde_json::Value,
+    pub training_files: Vec<OpenAIFile>,
+    pub validation_files: Vec<OpenAIFile>,
+    pub result_files: Vec<OpenAIFile>,
+    pub events: Option<FineTuneEvent>,
+}
+
+pub struct FineTuneEvent {
+    pub object: String,
+    pub created_at: u32,
+    pub level: String,
+    pub message: String,
+}
+
+pub struct ListEnginesResponse {
+    pub object: String,
+    pub data: Vec<Engine>,
+}
+
+pub struct ListModelResponse {
+    pub object: String,
+    pub data: Vec<Model>,
+}
+
+pub struct DeleteModelResponse {
+    pub id: String,
+    pub object: String,
+    pub deleted: bool,
+}
+
+#[derive(Serialize)]
+pub struct CreateCompletionRequest {
+    /// ID of the model to use. You can use the [List models](/docs/api-reference/models/list) API to see all of your available models, or see our [Model overview](/docs/models/overview) for descriptions of them.
+    pub model: String,
+
+    /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
+    ///
+    /// Note that <|endoftext|> is the document separator that the model sees during training, so if a prompt is not specified the model will generate as if from the beginning of a new document.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub prompt: Option<String>, // todo check type
+
+    /// The suffix that comes after a completion of inserted text.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub suffix: Option<String>, // todo: default null
+
+    /// The maximum number of [tokens](/tokenizer) to generate in the completion.
+    ///
+    /// The token count of your prompt plus `max_tokens` cannot exceed the model's context length. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_tokens: Option<u16>,
+
+    /// What [sampling temperature](https://towardsdatascience.com/how-to-sample-from-language-models-682bceb97277) to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
+    ///
+    /// We generally recommend altering this or `top_p` but not both.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub temperature: Option<f32>, // todo: min:0 ,max: 2, default: 1,
+
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    ///  We generally recommend altering this or `temperature` but not both.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub top_p: Option<f32>, //  todo: min: 0, max: 1, default: 1
+
+    /// How many completions to generate for each prompt.
+
+    /// **Note:** Because this parameter generates many completions, it can quickly consume your token quota. Use carefully and ensure that you have reasonable settings for `max_tokens` and `stop`.
+    ///
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub n: Option<u8>, // min:1 max: 128, default: 1
+
+    /// Whether to stream back partial progress. If set, tokens will be sent as data-only [server-sent events](https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#Event_stream_format)
+    /// as they become available, with the stream terminated by a `data: [DONE]` message.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stream: Option<bool>,
+
+    /// Include the log probabilities on the `logprobs` most likely tokens, as well the chosen tokens. For example, if `logprobs` is 5, the API will return a list of the 5 most likely tokens. The API will always return the `logprob` of the sampled token, so there may be up to `logprobs+1` elements in the response.
+
+    /// The maximum value for `logprobs` is 5. If you need more than this, please contact us through our [Help center](https://help.openai.com) and describe your use case.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub logprobs: Option<u8>, // min:0 , max: 5, default: null
+
+    /// Echo back the prompt in addition to the completion
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub echo: Option<bool>,
+
+    ///  Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stop: Option<String>, //todo: type?
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    ///
+    /// [See more information about frequency and presence penalties.](/docs/api-reference/parameter-details)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub presence_penalty: Option<f32>, // min: -2.0, max: 2.0, default 0
+
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    ///
+    /// [See more information about frequency and presence penalties.](/docs/api-reference/parameter-details)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub frequency_penalty: Option<f32>, // min: -2.0, max: 2.0, default: 0
+
+    /// Generates `best_of` completions server-side and returns the "best" (the one with the highest log probability per token). Results cannot be streamed.
+    ///
+    /// When used with `n`, `best_of` controls the number of candidate completions and `n` specifies how many to return – `best_of` must be greater than `n`.
+    ///
+    /// **Note:** Because this parameter generates many completions, it can quickly consume your token quota. Use carefully and ensure that you have reasonable settings for `max_tokens` and `stop`.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub best_of: Option<u8>, //min: 0, max: 20, default: 1
+
+    /// Modify the likelihood of specified tokens appearing in the completion.
+    ///
+    /// Accepts a json object that maps tokens (specified by their token ID in the GPT tokenizer) to an associated bias value from -100 to 100. You can use this [tokenizer tool](/tokenizer?view=bpe) (which works for both GPT-2 and GPT-3) to convert text to token IDs. Mathematically, the bias is added to the logits generated by the model prior to sampling. The exact effect will vary per model, but values between -1 and 1 should decrease or increase likelihood of selection; values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
+    ///
+    /// As an example, you can pass `{"50256": -100}` to prevent the <|endoftext|> token from being generated.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub logit_bias: Option<HashMap<String, serde_json::Value>>, // default: null
+
+    /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. [Learn more](/docs/usage-policies/end-user-ids).
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub user: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Logprobs {
+    pub tokens: Vec<String>,
+    pub token_logprobs: Vec<f32>,
+    pub token_offset: Vec<u32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Choice {
+    pub text: String,
+    pub index: u32,
+    pub logprobs: Option<Logprobs>,
+    pub finish_reason: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Usage {
+    pub prompt_tokens: u32,
+    pub completion_tokens: u32,
+    pub total_tokens: u32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateCompletionResponse {
+    pub id: String,
+    pub object: String,
+    pub created: u32,
+    pub model: String,
+    pub choices: Vec<Choice>,
+    pub usage: Option<Usage>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CreateEditRequest {
+    /// ID of the model to use. You can use the [List models](/docs/api-reference/models/list) API to see all of your available models, or see our [Model overview](/docs/models/overview) for descriptions of them.
+    pub model: String,
+
+    /// The input text to use as a starting point for the edit.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub input: Option<String>, // default ''
+
+    /// The instruction that tells the model how to edit the prompt.
+    pub instruction: String,
+
+    /// How many edits to generate for the input and instruction.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub n: Option<u8>, // min:1 max: 20 default:1
+
+    /// What [sampling temperature](https://towardsdatascience.com/how-to-sample-from-language-models-682bceb97277) to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
+    ///
+    /// We generally recommend altering this or `top_p` but not both.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub temperature: Option<f32>, // todo: min:0 ,max: 2, default: 1,
+
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    ///  We generally recommend altering this or `temperature` but not both.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub top_p: Option<f32>, //  todo: min: 0, max: 1, default: 1
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateEditResponse {
+    pub id: String,
+    pub object: String,
+    pub created: u32,
+    pub model: String,
+    pub choices: Vec<Choice>,
+    pub usage: Usage,
+}
+
+#[derive(Default, Debug, Serialize)]
+pub enum ImageSize {
+    #[serde(rename = "256x256")]
+    S256x256,
+    #[serde(rename = "512x512")]
+    S512x512,
+    #[default]
+    #[serde(rename = "1024x1024")]
+    S1024x1024,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ResponseFormat {
+    Url,
+    B64Json,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CreateImageRequest {
+    /// A text description of the desired image(s). The maximum length is 1000 characters.
+    pub prompt: String,
+
+    /// The number of images to generate. Must be between 1 and 10.
+    pub n: Option<u8>, // min:1 max:10 default:1
+
+    /// The size of the generated images. Must be one of `256x256`, `512x512`, or `1024x1024`.
+    pub size: Option<ImageSize>,
+
+    /// The format in which the generated images are returned. Must be one of `url` or `b64_json`.
+    pub response_format: Option<ResponseFormat>,
+
+    /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. [Learn more](/docs/usage-policies/end-user-ids).
+    pub user: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ImageData {
+    Url(String),
+    #[serde(rename = "b64_json")]
+    B64Json(String),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ImageResponse {
+    pub created: u32,
+    pub data: Vec<ImageData>,
+}
+
+pub struct Image {
+    pub name: String,
+    pub data: Vec<u8>, // todo: stream?
+}
+
+pub struct CreateImageEditRequest {
+    /// The image to edit. Must be a valid PNG file, less than 4MB, and square.
+    pub image: Image,
+
+    /// An additional image whose fully transparent areas (e.g. where alpha is zero) indicate where `image` should be edited. Must be a valid PNG file, less than 4MB, and have the same dimensions as `image`.
+    pub mask: Image,
+
+    /// A text description of the desired image(s). The maximum length is 1000 characters.
+    pub prompt: String,
+
+    /// The number of images to generate. Must be between 1 and 10.
+    pub n: Option<u8>, // min:1 max:10 default:1
+
+    /// The size of the generated images. Must be one of `256x256`, `512x512`, or `1024x1024`.
+    pub size: Option<ImageSize>,
+
+    /// The format in which the generated images are returned. Must be one of `url` or `b64_json`.
+    pub response_format: Option<ResponseFormat>,
+
+    /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. [Learn more](/docs/usage-policies/end-user-ids).
+    pub user: Option<String>,
+}
+
+pub struct CreateImageVariationRequest {
+    /// The image to use as the basis for the variation(s). Must be a valid PNG file, less than 4MB, and square.
+    pub image: Image,
+
+    /// The number of images to generate. Must be between 1 and 10.
+    pub n: Option<u8>, // min:1 max:10 default:1
+
+    /// The size of the generated images. Must be one of `256x256`, `512x512`, or `1024x1024`.
+    pub size: Option<ImageSize>,
+
+    /// The format in which the generated images are returned. Must be one of `url` or `b64_json`.
+    pub response_format: Option<ResponseFormat>,
+
+    /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. [Learn more](/docs/usage-policies/end-user-ids).
+    pub user: Option<String>,
+}
