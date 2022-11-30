7e1b5b8429e3a82d14390aa8569dfc6e44d7dd28
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-11-30 03:13:33 -0800

    dedup code

--- async-openai/src/image.rs
+++ async-openai/src/image.rs
@@ -11,25 +11,6 @@ impl Image {
         client: &Client,
         request: CreateImageRequest,
     ) -> Result<ImageResponse, OpenAIError> {
-        let response = reqwest::Client::new()
-            .post(format!("{}/images/generations", client.api_base()))
-            .bearer_auth(client.api_key())
-            .json(&request)
-            .send()
-            .await?;
-
-        let status = response.status();
-        let bytes = response.bytes().await?;
-
-        if !status.is_success() {
-            let wrapped_error: crate::error::WrappedError =
-                serde_json::from_slice(bytes.as_ref()).map_err(OpenAIError::JSONDeserialize)?;
-
-            return Err(OpenAIError::ApiError(wrapped_error.error));
-        }
-
-        let response: ImageResponse =
-            serde_json::from_slice(bytes.as_ref()).map_err(OpenAIError::JSONDeserialize)?;
-        Ok(response)
+        client.execute("/images/generations", request).await
     }
 }
