9641d54175f845bd286a307b0f4ac9b9fafc4c3f
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-01 01:44:48 -0800

    example to list and retrieve models

---
+++ examples/models/src/main.rs
@@ -0,0 +1,13 @@
+use async_openai as openai;
+use openai::{Client, Models};
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    let response = Models::list(&client).await;
+
+    println!("Models::list:\n {:#?}", response);
+
+    let response = Models::retrieve(&client, "text-davinci-003").await;
+    println!("Retrieved text-davinci-003: {response:#?}");
+}
