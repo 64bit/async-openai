4c5f93a2db291690f91a90827d8e35634bdd29fb
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-01 00:58:22 -0800

    create edit example

---
+++ examples/create-edit/src/main.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+
+use async_openai as openai;
+use openai::{types::CreateEditRequest, Client, Edit};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
+    let request = CreateEditRequest {
+        model: "text-davinci-edit-001".to_string(),
+        input: Some(
+            "It's surely our responsibility to do everything within our power
+            to create a planet that provides a home not just for us,
+            but for all life on Earth."
+                .into(),
+        ),
+        instruction: "Make this in the voice of Sir David Attenborough".to_string(),
+        n: Some(2),
+        temperature: Some(0.9), // We generally recommend altering this or top_p but not both.
+        top_p: None,
+    };
+
+    let response = Edit::create(&client, request).await?;
+
+    println!("response: {:#?}", response);
+
+    Ok(())
+}
