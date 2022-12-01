5977c414efee4745f6ec07c7831b32aaf93d8b9b
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-01 00:09:58 -0800

    Create Image variations (#5)
    * create image variation
    
    * exampl for create-image-variation

---
+++ examples/create-image-variation/src/main.rs
@@ -0,0 +1,26 @@
+use std::error::Error;
+
+use async_openai as openai;
+use openai::{
+    types::{CreateImageVariationRequest, ImageInput, ImageSize, ResponseFormat},
+    Client, Image,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
+    let request = CreateImageVariationRequest {
+        image: ImageInput::new("./images/cake.png"),
+        n: Some(1),
+        size: Some(ImageSize::S256x256),
+        response_format: Some(ResponseFormat::Url),
+        user: Some("async-openai".to_string()),
+    };
+
+    let response = Image::create_variation(&client, request).await?;
+
+    response.save("./data").await?;
+
+    Ok(())
+}
