0d36b1ebba7b796b79dc1f48785c367dbfa2fa4b
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-04 02:45:37 -0800

    working example and add readme to add output of this example

--- examples/codex/src/main.rs
+++ examples/codex/src/main.rs
@@ -5,9 +5,8 @@ use openai::{types::CreateCompletionRequest, Client, Completion};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let prompt = "
-    /// Rust language
-    /// Download a url and save it to filesystem. No tests.";
+    let prompt = "/// Rust language
+/// Function to download a url and save it disk.";
 
     let client = Client::new();
 
@@ -15,16 +14,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         model: "code-davinci-002".to_owned(),
         prompt: Some(prompt.to_owned()),
         max_tokens: Some(256),
+        temperature: Some(0.0),
         top_p: Some(1.0),
         best_of: Some(1),
         logprobs: Some(1),
         echo: Some(true),
+        frequency_penalty: Some(0.0),
+        presence_penalty: Some(0.0),
         ..Default::default()
     };
 
     let response = Completion::create(&client, request).await?;
-
-    println!("{response:#?}");
 
     let choice = response.choices.iter().nth(0).unwrap();
 
