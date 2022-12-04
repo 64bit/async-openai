da1fbb475003ca95cc8809af0445a7572f43c48d
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-04 02:52:49 -0800

    Add Codex example (Rust language Function to download a url and save it to disk) (#17)
    * incomplete; wip
    
    * update and fix Logprobs struct
    
    * remove println
    
    * working example and add readme to add output of this example
    
    * update prompt english and update output

---
+++ examples/codex/src/main.rs
@@ -0,0 +1,34 @@
+use std::error::Error;
+
+use async_openai as openai;
+use openai::{types::CreateCompletionRequest, Client, Completion};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let prompt = "/// Rust language
+/// Function to download a url and save it to disk.";
+
+    let client = Client::new();
+
+    let request = CreateCompletionRequest {
+        model: "code-davinci-002".to_owned(),
+        prompt: Some(prompt.to_owned()),
+        max_tokens: Some(256),
+        temperature: Some(0.0),
+        top_p: Some(1.0),
+        best_of: Some(1),
+        logprobs: Some(1),
+        echo: Some(true),
+        frequency_penalty: Some(0.0),
+        presence_penalty: Some(0.0),
+        ..Default::default()
+    };
+
+    let response = Completion::create(&client, request).await?;
+
+    let choice = response.choices.iter().nth(0).unwrap();
+
+    println!("{}", choice.text);
+
+    Ok(())
+}
