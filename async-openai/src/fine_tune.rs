08b5518b95e9b6a882326c389a2eb2a01e8f8550
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-17 22:24:21 -0800

    Fine tunes (#24)
    * list, retrieve, cancel, list_events, and delete model for Fine Tuning
    
    * Create Fine Tune

---
+++ async-openai/src/fine_tune.rs
@@ -0,0 +1,55 @@
+use crate::{
+    error::OpenAIError,
+    types::{CreateFineTuneRequest, FineTune, ListFineTuneEventsResponse, ListFineTuneResponse},
+    Client,
+};
+
+/// Manage fine-tuning jobs to tailor a model to your specific training data.
+///
+/// Related guide: [Fine-tune models](https://beta.openai.com/docs/guides/fine-tuning)
+pub struct FineTunes;
+
+impl FineTunes {
+    /// Creates a job that fine-tunes a specified model from a given dataset.
+    ///
+    /// Response includes details of the enqueued job including job status and the name of the fine-tuned models once complete.
+    ///
+    /// [Learn more about Fine-tuning](https://beta.openai.com/docs/guides/fine-tuning)
+    pub async fn create(
+        client: &Client,
+        request: CreateFineTuneRequest,
+    ) -> Result<FineTune, OpenAIError> {
+        client.post("/fine-tunes", request).await
+    }
+
+    /// List your organization's fine-tuning jobs
+    pub async fn list(client: &Client) -> Result<ListFineTuneResponse, OpenAIError> {
+        client.get("/fine-tunes").await
+    }
+
+    /// Gets info about the fine-tune job.
+    ///
+    /// [Learn more about Fine-tuning](https://beta.openai.com/docs/guides/fine-tuning)
+    pub async fn retrieve(client: &Client, fine_tune_id: &str) -> Result<FineTune, OpenAIError> {
+        client
+            .get(format!("/fine-tunes/{fine_tune_id}").as_str())
+            .await
+    }
+
+    /// Immediately cancel a fine-tune job.
+    pub async fn cancel(client: &Client, fine_tune_id: &str) -> Result<FineTune, OpenAIError> {
+        client
+            .post(format!("/fine-tunes/{fine_tune_id}/cancel").as_str(), ())
+            .await
+    }
+
+    /// Get fine-grained status updates for a fine-tune job.
+    pub async fn list_events(
+        client: &Client,
+        fine_tune_id: &str,
+    ) -> Result<ListFineTuneEventsResponse, OpenAIError> {
+        client
+            .get(format!("/fine-tunes/{fine_tune_id}/events").as_str())
+            .await
+    }
+}
--- async-openai/src/types.rs
+++ async-openai/src/types.rs
@@ -515,23 +515,133 @@ pub struct OpenAIFile {
     pub status_details: Option<serde_json::Value>, // nullable: true
 }
 
-/* Not used yet
+#[derive(Debug, Serialize)]
+pub struct CreateFineTuneRequest {
+    /// The ID of an uploaded file that contains training data.
+    ///
+    /// See [upload file](https://beta.openai.com/docs/api-reference/files/upload) for how to upload a file.
+    ///
+    /// Your dataset must be formatted as a JSONL file, where each training
+    /// example is a JSON object with the keys "prompt" and "completion".
+    /// Additionally, you must upload your file with the purpose `fine-tune`.
+    ///
+    /// See the [fine-tuning guide](https://beta.openai.com/docs/guides/fine-tuning/creating-training-data) for more details.
+    pub training_file: String,
+
+    /// The ID of an uploaded file that contains validation data.
+    ///
+    /// If you provide this file, the data is used to generate validation
+    /// metrics periodically during fine-tuning. These metrics can be viewed in
+    /// the [fine-tuning results file](https://beta.openai.com/docs/guides/fine-tuning/analyzing-your-fine-tuned-model).
+    /// Your train and validation data should be mutually exclusive.
+    ///
+    /// Your dataset must be formatted as a JSONL file, where each validation
+    /// example is a JSON object with the keys "prompt" and "completion".
+    /// Additionally, you must upload your file with the purpose `fine-tune`.
+    ///
+    /// See the [fine-tuning guide](https://beta.openai.com/docs/guides/fine-tuning/creating-training-data) for more details.
+    pub validation_file: Option<String>,
+
+    /// The name of the base model to fine-tune. You can select one of "ada",
+    /// "babbage", "curie", "davinci", or a fine-tuned model created after 2022-04-21.
+    /// To learn more about these models, see the [Models](https://beta.openai.com/docs/models) documentation.
+    pub model: Option<String>,
+
+    /// The number of epochs to train the model for. An epoch refers to one
+    /// full cycle through the training dataset.
+    pub n_epochs: Option<u32>, // default: 4
+
+    /// The batch size to use for training. The batch size is the number of
+    /// training examples used to train a single forward and backward pass.
+    ///
+    /// By default, the batch size will be dynamically configured to be
+    /// ~0.2% of the number of examples in the training set, capped at 256 -
+    /// in general, we've found that larger batch sizes tend to work better
+    /// for larger datasets.
+    pub batch_size: Option<u32>, // default: null
+
+    /// The learning rate multiplier to use for training.
+    /// The fine-tuning learning rate is the original learning rate used for
+    /// pretraining multiplied by this value.
+    ///
+    /// By default, the learning rate multiplier is the 0.05, 0.1, or 0.2
+    /// depending on final `batch_size` (larger learning rates tend to
+    /// perform better with larger batch sizes). We recommend experimenting
+    /// with values in the range 0.02 to 0.2 to see what produces the best
+    /// results.
+    pub learning_rate_multiplier: Option<f32>, // default: null
+
+    /// The weight to use for loss on the prompt tokens. This controls how
+    /// much the model tries to learn to generate the prompt (as compared
+    /// to the completion which always has a weight of 1.0), and can add
+    /// a stabilizing effect to training when completions are short.
+    ///
+    /// If prompts are extremely long (relative to completions), it may make
+    /// sense to reduce this weight so as to avoid over-prioritizing
+    /// learning the prompt.
+    pub prompt_loss_weight: Option<f32>, // default: 0.01
+
+    /// If set, we calculate classification-specific metrics such as accuracy
+    /// and F-1 score using the validation set at the end of every epoch.
+    /// These metrics can be viewed in the [results file](https://beta.openai.com/docs/guides/fine-tuning/analyzing-your-fine-tuned-model).
+    ///
+    /// In order to compute classification metrics, you must provide a
+    /// `validation_file`. Additionally, you must
+    /// specify `classification_n_classes` for multiclass classification or
+    /// `classification_positive_class` for binary classification.
+    pub compute_classification_metrics: Option<bool>, // default: false
+
+    /// The number of classes in a classification task.
+    ///
+    /// This parameter is required for multiclass classification.
+    pub classification_n_classes: Option<u32>, // default: null
+
+    /// The positive class in binary classification.
+    ///
+    /// This parameter is needed to generate precision, recall, and F1
+    /// metrics when doing binary classification.
+    pub classification_positive_class: Option<String>, // default: null
+
+    /// If this is provided, we calculate F-beta scores at the specified
+    /// beta values. The F-beta score is a generalization of F-1 score.
+    /// This is only used for binary classification.
+    ///
+    /// With a beta of 1 (i.e. the F-1 score), precision and recall are
+    /// given the same weight. A larger beta score puts more weight on
+    /// recall and less on precision. A smaller beta score puts more weight
+    /// on precision and less on recall.
+    pub classification_betas: Option<Vec<f32>>, // default: null
+
+    /// A string of up to 40 characters that will be added to your fine-tuned model name.
+    ///
+    /// For example, a `suffix` of "custom-model-name" would produce a model name like `ada:ft-your-org:custom-model-name-2022-02-15-04-21-04`.
+    pub suffix: Option<String>, // default: null, minLength:1, maxLength:40
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ListFineTuneResponse {
+    pub object: String,
+    pub data: Vec<FineTune>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct FineTune {
     pub id: String,
     pub object: String,
     pub created_at: u32,
     pub updated_at: u32,
     pub model: String,
-    pub fine_tuned_model: String, // nullable: true
+    pub fine_tuned_model: Option<String>, // nullable: true
     pub organization_id: String,
     pub status: String,
     pub hyperparams: serde_json::Value,
     pub training_files: Vec<OpenAIFile>,
     pub validation_files: Vec<OpenAIFile>,
     pub result_files: Vec<OpenAIFile>,
-    pub events: Option<FineTuneEvent>,
+    pub events: Option<Vec<FineTuneEvent>>,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct FineTuneEvent {
     pub object: String,
     pub created_at: u32,
@@ -539,9 +649,15 @@ pub struct FineTuneEvent {
     pub message: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct ListFineTuneEventsResponse {
+    pub object: String,
+    pub data: Vec<FineTuneEvent>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct DeleteModelResponse {
     pub id: String,
     pub object: String,
     pub deleted: bool,
 }
-*/
