20f583bb8d8927a6a1c309d544e317f1a557906a
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-30 02:17:02 -0800

    Update Models example to use ergonomic builder pattern

--- examples/models/src/main.rs
+++ examples/models/src/main.rs
@@ -1,12 +1,17 @@
-use async_openai::{Client, Models};
+use std::error::Error;
+
+use async_openai::Client;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new();
-    let response = Models::list(&client).await;
 
-    println!("Models::list:\n {:#?}", response);
+    let model_list = client.models().list().await?;
 
-    let response = Models::retrieve(&client, "text-davinci-003").await;
-    println!("Retrieved text-davinci-003: {response:#?}");
+    println!("List of models:\n {:#?}", model_list.data);
+
+    let model = client.models().retrieve("text-davinci-003").await?;
+    println!("text-davinci-003 model id: {}", model.id);
+
+    Ok(())
 }
