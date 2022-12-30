4a4e7817bc01993b8e868afa639b7d67a1b58b6d
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-12-30 02:07:20 -0800

    Update Completions Prompt macro test

--- async-openai/tests/completion.rs
+++ async-openai/tests/completion.rs
@@ -1,58 +1,47 @@
 //! This test is primarily to make sure that macros_rules for From traits are correct.
-use async_openai::{
-    error::OpenAIError,
-    types::{CreateCompletionRequest, CreateCompletionRequestArgs, Prompt},
-};
+use async_openai::types::Prompt;
 
-fn request<T>(prompt: T) -> Result<CreateCompletionRequest, OpenAIError>
+fn prompt_input<T>(input: T) -> Prompt
 where
     Prompt: From<T>,
 {
-    CreateCompletionRequestArgs::default()
-        .prompt(prompt)
-        .max_tokens(10_u16)
-        .temperature(1.0)
-        .n(1)
-        .stream(false)
-        .stop("stop")
-        .user("async-openai")
-        .build()
+    input.into()
 }
 
 #[test]
-fn create_completion_request() {
+fn create_prompt_input() {
     let prompt = "This is &str prompt";
-    let _ = request(prompt);
+    let _ = prompt_input(prompt);
 
     let prompt = "This is String".to_string();
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let prompt = vec!["This is first", "This is second"];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let prompt = vec!["First string".to_string(), "Second string".to_string()];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let first = "First".to_string();
     let second = "Second".to_string();
     let prompt = vec![&first, &second];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let prompt = ["first", "second"];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let prompt = ["first".to_string(), "second".to_string()];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 
     let first = "First".to_string();
     let second = "Second".to_string();
     let prompt = [&first, &second];
-    let _ = request(&prompt);
-    let _ = request(prompt);
+    let _ = prompt_input(&prompt);
+    let _ = prompt_input(prompt);
 }
