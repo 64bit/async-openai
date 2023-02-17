d96faab0d0bfc24726f903e32ac38ef291c2d994
luketpeterson <36806965+luketpeterson@users.noreply.github.com>
2023-02-18 08:06:16 +0900

    Adding Send bound to boxed future types (#37)
    * Adding Send bound to boxed future types so that they will work in a multithreaded runtime
    
    * Update async-openai/tests/boxed_future.rs
    
    ---------
    
    Co-authored-by: Himanshu Neema <himanshun.iitkgp@gmail.com>

---
+++ async-openai/tests/boxed_future.rs
@@ -0,0 +1,44 @@
+
+use futures::StreamExt;
+use futures::future::{BoxFuture, FutureExt};
+
+use async_openai::Client;
+use async_openai::types::{CompletionResponseStream, CreateCompletionRequestArgs};
+
+#[tokio::test]
+async fn boxed_future_test() {
+
+    fn interpret_bool(token_stream: &mut CompletionResponseStream) -> BoxFuture<'_, bool> {
+        async move {
+            while let Some(response) = token_stream.next().await {
+                match response {
+                    Ok(response) => {
+                        let token_str = &response.choices[0].text.trim();
+                        if !token_str.is_empty() {
+                            return token_str.contains("yes") || token_str.contains("Yes");
+                        }
+                    },
+                    Err(e) => eprintln!("Error: {e}"),
+                }
+            }
+            false
+        }.boxed()
+    }
+
+    let client = Client::new();
+
+    let request = CreateCompletionRequestArgs::default()
+        .model("text-babbage-001")
+        .n(1)
+        .prompt("does 2 and 2 add to four? (yes/no):\n")
+        .stream(true)
+        .logprobs(3)
+        .max_tokens(64_u16)
+        .build().unwrap();
+
+    let mut stream = client.completions().create_stream(request).await.unwrap();
+
+    let result = interpret_bool(&mut stream).await;
+    assert_eq!(result, true);
+
+}
