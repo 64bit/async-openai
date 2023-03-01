5a994820275837e3fed9e75ea301df34cd219526
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-03-01 11:59:10 -0800

    oops: example for chat completion

---
+++ examples/chat/README.md
@@ -0,0 +1,5 @@
+### Output
+
+> Response:
+>
+> 0: Role: assistant  Content: The 2020 World Series was played at Globe Life Field in Arlington, Texas.
---
+++ examples/chat/src/main.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+
+use async_openai::{
+    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    Client,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo")
+        .messages([
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::System)
+                .content("You are a helpful assistant.")
+                .build()?,
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content("Who won the world series in 2020?")
+                .build()?,
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::Assistant)
+                .content("The Los Angeles Dodgers won the World Series in 2020.")
+                .build()?,
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content("Where was it played?")
+                .build()?,
+        ])
+        .build()?;
+
+    let response = client.chat().create(request).await?;
+
+    println!("\nResponse:\n");
+    for choice in response.choices {
+        println!(
+            "{}: Role: {}  Content: {}",
+            choice.index, choice.message.role, choice.message.content
+        );
+    }
+
+    Ok(())
+}
