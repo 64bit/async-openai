22b07157fc43f9098b052dc1be6a03799ec3f43d
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-03-01 12:31:36 -0800

    chat streaming example

---
+++ examples/chat-stream/README.md
@@ -0,0 +1,46 @@
+### Output
+
+```
+Verse 1 (Chris Martin):
+The sun rises over the horizon
+I feel a new day has come
+The warmth on my face, I'm alive
+I hear the sound of a new song
+
+Chorus (Chris Martin and AR Rahman):
+So come let's sing together
+With our hearts and our souls
+Our voices will unite forever
+In a harmony we'll never let go
+
+Verse 2 (AR Rahman):
+We come from different worlds
+But music knows no bounds
+Our melodies will intertwine
+In a rhythm that astounds
+
+Chorus (Chris Martin and AR Rahman):
+So come let's sing together
+With our hearts and our souls
+Our voices will unite forever
+In a harmony we'll never let go
+
+Bridge (AR Rahman):
+We'll dance beneath the stars
+And let our spirits soar
+Our hearts will beat as one
+As we sing forevermore
+
+Chorus (Chris Martin and AR Rahman):
+So come let's sing together
+With our hearts and our souls
+Our voices will unite forever
+In a harmony we'll never let go
+
+Outro (Chris Martin and AR Rahman):
+Our love for music is our bond
+And it will never fade
+Together we will journey on
+In a symphony that's never played.
+
+```
---
+++ examples/chat-stream/src/main.rs
@@ -0,0 +1,39 @@
+use std::error::Error;
+
+use async_openai::{
+    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    Client,
+};
+use futures::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo")
+        .messages([ChatCompletionRequestMessageArgs::default()
+            .content("write a song if Coldplay and AR Rahman collaborated together")
+            .role(Role::User)
+            .build()?])
+        .build()?;
+
+    let mut stream = client.chat().create_stream(request).await?;
+
+    while let Some(result) = stream.next().await {
+        match result {
+            Ok(response) => {
+                response.choices.iter().for_each(|chat_choice| {
+                    if let Some(ref content) = chat_choice.delta.content {
+                        print!("{}", content);
+                    }
+                });
+            }
+            Err(err) => {
+                println!("error: {err}");
+            }
+        }
+    }
+
+    Ok(())
+}
