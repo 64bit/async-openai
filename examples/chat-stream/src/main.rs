a0b93a70b7ed758f968f6a3db5c545d0a9488e7e
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-03-11 19:35:54 -0800

    update chat-stream example

--- examples/chat-stream/src/main.rs
+++ examples/chat-stream/src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::io::{stdout, Write};
 
 use async_openai::{
     types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
@@ -14,27 +15,41 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .model("gpt-3.5-turbo")
         .max_tokens(1024u16)
         .messages([ChatCompletionRequestMessageArgs::default()
-            .content("write a song if Coldplay and AR Rahman collaborated together")
+            .content("Write a marketing blog praising and introducing Rust library async-openai")
             .role(Role::User)
             .build()?])
         .build()?;
 
     let mut stream = client.chat().create_stream(request).await?;
 
-    // For reasons not documented in OpenAI docs / OpenAPI spec, the response of streaming call is different and doesn't include all the same fields.
+    // For reasons not documented in OpenAI docs / OpenAPI spec,
+    // the response of streaming call is different and doesn't include all the same fields.
+
+    // From Rust docs on print: https://doc.rust-lang.org/std/macro.print.html
+    //
+    //  Note that stdout is frequently line-buffered by default so it may be necessary
+    //  to use io::stdout().flush() to ensure the output is emitted immediately.
+    //
+    //  The print! macro will lock the standard output on each call.
+    //  If you call print! within a hot loop, this behavior may be the bottleneck of the loop.
+    //  To avoid this, lock stdout with io::stdout().lock():
+
+
+    let mut lock = stdout().lock();
     while let Some(result) = stream.next().await {
         match result {
             Ok(response) => {
                 response.choices.iter().for_each(|chat_choice| {
                     if let Some(ref content) = chat_choice.delta.content {
-                        print!("{}", content);
+                        write!(lock, "{}", content).unwrap();
                     }
                 });
             }
             Err(err) => {
-                println!("error: {err}");
+                writeln!(lock, "error: {err}").unwrap();
             }
         }
+        stdout().flush()?;
     }
 
     Ok(())
