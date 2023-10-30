703549f977f75bdfac731b29ea5199da25f22c72
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-10-29 22:41:00 -0700

    Update async-openai/src/types/types.rs

--- async-openai/src/types/types.rs
+++ async-openai/src/types/types.rs
@@ -784,7 +784,10 @@ pub struct CreateChatCompletionRequest {
     pub functions: Option<Vec<ChatCompletionFunctions>>,
 
     /// Controls how the model responds to function calls.
-    /// None is the default when no functions are present. Auto is the default if functions are present.
+    /// "none" means the model does not call a function, and responds to the end-user.
+    /// "auto" means the model can pick between an end-user or calling a function.
+    /// Specifying a particular function via `{"name":\ "my_function"}` forces the model to call that function.
+    /// "none" is the default when no functions are present. "auto" is the default if functions are present.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub function_call: Option<ChatCompletionFunctionCall>,
 
