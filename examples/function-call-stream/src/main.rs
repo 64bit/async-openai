7bae07e0a12a444203af685f9e56589b8876d54a
Himanshu Neema <himanshun.iitkgp@gmail.com>
2023-11-06 20:10:17 -0800

    fix function call examples

--- examples/function-call-stream/src/main.rs
+++ examples/function-call-stream/src/main.rs
@@ -2,12 +2,11 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::io::{stdout, Write};
 
-use async_openai::types::FinishReason;
+use async_openai::types::{
+    ChatCompletionRequestFunctionMessageArgs, ChatCompletionRequestUserMessageArgs, FinishReason,
+};
 use async_openai::{
-    types::{
-        ChatCompletionFunctionsArgs, ChatCompletionRequestMessageArgs,
-        CreateChatCompletionRequestArgs, Role,
-    },
+    types::{ChatCompletionFunctionsArgs, CreateChatCompletionRequestArgs, Role},
     Client,
 };
 
@@ -22,10 +21,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let request = CreateChatCompletionRequestArgs::default()
         .max_tokens(512u16)
         .model("gpt-3.5-turbo-0613")
-        .messages([ChatCompletionRequestMessageArgs::default()
-            .role(Role::User)
+        .messages([ChatCompletionRequestUserMessageArgs::default()
             .content("What's the weather like in Boston?")
-            .build()?])
+            .build()?
+            .into()])
         .functions([ChatCompletionFunctionsArgs::default()
             .name("get_current_weather")
             .description("Get the current weather in a given location")
@@ -99,15 +98,15 @@ async fn call_fn(
     let function_response = function(location, unit); // call the function
 
     let message = vec![
-        ChatCompletionRequestMessageArgs::default()
-            .role(Role::User)
+        ChatCompletionRequestUserMessageArgs::default()
             .content("What's the weather like in Boston?")
-            .build()?,
-        ChatCompletionRequestMessageArgs::default()
-            .role(Role::Function)
+            .build()?
+            .into(),
+        ChatCompletionRequestFunctionMessageArgs::default()
             .content(function_response.to_string())
-            .name(name.clone())
-            .build()?,
+            .name(name)
+            .build()?
+            .into(),
     ];
 
     let request = CreateChatCompletionRequestArgs::default()
