f0ebf7ef53acb194097f2940e6130d0419c57186
Gravel Hill <StrangeKnoll@gmail.com>
2023-11-13 17:12:06 -0500

    Update to Assistants example

--- examples/assistants/README.md
+++ examples/assistants/README.md
@@ -1,3 +1,9 @@
-### Help Wanted
+### Assistants
+Note: before you run this example edit the following lines in main.rs respectively:
+```let KEY = "YOUR KEY HERE"```
+```let bot_id = "Your Assistand ID Here"```
 
-This is a placeholder example, a self contained to simply run with `cargo run` from community is welcome!
+This example allows you to chat with an assistant, and walks through the process of:
+- creating threads, messages, and run requests
+- appending messages and retrieving messages from the thread
+- running threads and retrieving their responses
--- examples/assistants/src/main.rs
+++ examples/assistants/src/main.rs
@@ -1,88 +1,131 @@
 use async_openai::{
-    types::{CreateMessageRequestArgs, CreateRunRequestArgs, CreateThreadRequestArgs},
-    Client,
+    types::{CreateMessageRequestArgs, CreateRunRequestArgs, CreateThreadRequestArgs, CreateThreadAndRunRequestArgs, CreateThreadAndRunRequest, CreateChatCompletionRequestArgs, ChatCompletionRequestSystemMessageArgs, Role, RunStatus, MessageContent, MessageContentTextObject},
+    Client, config::OpenAIConfig,
 };
 use std::error::Error;
-use tracing_subscriber::{
-    prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
-};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer())
-        .with(EnvFilter::from_default_env())
-        .init();
+    let query = [("limit", "1")]; //limit the list responses to 1 message
 
-    let client = Client::new();
+    //our api key
+    let KEY = "YOUR KEY HERE";
 
-    let query = [("limit", "10")];
+    //the id of the Assistant we want to use
+    let rust_bot_id = "Your Assistant ID Here";
+    
+    //create a client with our api key
+    let config = OpenAIConfig::new().with_api_key(KEY);
+    let client = Client::with_config(config);
 
-    let assistants = client.assistants().list(&query).await?;
-    println!("assistants: {assistants:#?}");
+    //create a thread for the conversation
+    let thread_request = CreateThreadRequestArgs::default().build()?;
+    let thread = client.threads().create(thread_request.clone()).await?;
 
-    let create_thread_request = CreateThreadRequestArgs::default().build()?;
-    let thread_object = client.threads().create(create_thread_request).await?;
+    loop{
+        println!("How can I help you?");
+        //get user input
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
 
-    println!("thread object: {thread_object:#?}");
+        //create a message for the thread
+        let message = CreateMessageRequestArgs::default()
+            .role("user")
+            .content(input.clone())
+            .build()?;
 
-    let create_message_request = CreateMessageRequestArgs::default().build()?;
-    let message_object = client
-        .threads()
-        .messages(&thread_object.id)
-        .create(create_message_request)
-        .await?;
+        //attach message to the thread
+        let message_obj = client
+            .threads()
+            .messages(&thread.id)
+            .create(message)
+            .await?;
 
-    println!("message object: {message_object:#?}");
+        //create a run for the thread
+        let run_request = CreateRunRequestArgs::default()
+            .assistant_id(rust_bot_id)
+            .build()?;
+        let run = client
+            .threads()
+            .runs(&thread.id)
+            .create(run_request)
+            .await?;
 
-    let create_run_request = CreateRunRequestArgs::default().build()?;
-    let run_object = client
-        .threads()
-        .runs(&thread_object.id)
-        .create(create_run_request)
-        .await;
+        //wait for the run to complete
+        let mut awaiting_response = true;
+        while awaiting_response {
+            //retrieve the run
+            let run = client
+                .threads()
+                .runs(&thread.id)
+                .retrieve(&run.id)
+                .await?;
+            //check the status of the run
+            match run.status {
+                RunStatus::Completed => {
+                    awaiting_response = false;
+                    // once the run is completed we
+                    // get the response from the run
+                    // which will be the first message
+                    //in the thread
 
-    println!("run object: {run_object:#?}");
+                    //retrieve the response from the run
+                    let response = client
+                        .threads()
+                        .messages(&thread.id)
+                        .list(&query)
+                        .await?;
+                    //get the message id from the response
+                    let message_id = response
+                        .data.get(0).unwrap()
+                        .id.clone();
+                    //get the message from the response
+                    let message = client
+                        .threads()
+                        .messages(&thread.id)
+                        .retrieve(&message_id)
+                        .await?;
+                    //get the content from the message
+                    let content = message
+                        .content.get(0).unwrap();
+                    //get the text from the content
+                    let text = match content {
+                        MessageContent::Text(text) => text.text.value.clone(),
+                        MessageContent::ImageFile(_) => panic!("imaged are not supported in the terminal"),
+                    };
+                    //print the text
+                    println!("Response: {}", text);
 
-    let thread_runs = client
-        .threads()
-        .runs(&thread_object.id)
-        .list(&query)
-        .await?;
-
-    println!("runs for thread id {}: {thread_runs:#?}", thread_object.id);
-
-    let thread_messages = client
-        .threads()
-        .messages(&thread_object.id)
-        .list(&query)
-        .await?;
-
-    println!("thread messages: {thread_messages:#?}");
-
-    let thread_message_files = client
-        .threads()
-        .messages(&thread_object.id)
-        .files("message_id")
-        .list(&query)
-        .await;
-    println!("thread message file: {thread_message_files:#?}");
-
-    let thread_run_steps = client
-        .threads()
-        .runs(&thread_object.id)
-        .steps("run_id")
-        .list(&query)
-        .await;
-
-    println!("thread run steps: {thread_run_steps:#?}");
-
-    let deleted_thread_object = client.threads().delete(&thread_object.id).await?;
-    println!("deleted thread object: {deleted_thread_object:#?}");
-
-    let assistant_files = client.assistants().files("assistant_id").list(&query).await;
-
-    println!("assistant files: {assistant_files:#?}");
-
+                }
+                RunStatus::Failed => {
+                    awaiting_response = false;
+                    println!("Run Failed: {:#?}", run);
+                }
+                RunStatus::Queued => {
+                    println!("Run Queued");
+                },
+                RunStatus::Cancelling => {
+                    println!("Run Cancelling");
+                },
+                RunStatus::Cancelled => {
+                    println!("Run Cancelled");
+                },
+                RunStatus::Expired => {
+                    println!("Run Expired");
+                },
+                RunStatus::RequiresAction => {
+                    println!("Run Requires Action");
+                },
+                RunStatus::InProgress => {
+                    println!("Waiting for response...");
+                },
+                _ => {
+                    println!("Waiting for response...");
+                }
+            }
+            //wait for 1 second before checking the status again
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    }
     Ok(())
 }
