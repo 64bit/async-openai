6141d5b55535dd20ad15e6975bce45b3fead2683
maple@max <feng.liang@kaust.edu.sa>
2023-11-19 18:30:16 +0800

    add OpenAI support in example

--- examples/openai-web-app/src/main.rs
+++ examples/openai-web-app/src/main.rs
@@ -4,6 +4,8 @@ use log::Level;
 
 use async_openai::types::{ChatCompletionRequestMessage, ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs, Role};
 
+const USE_AZURE: bool = false;
+
 const API_BASE: &str = "...";
 const API_KEY: &str = "...";
 const API_VERSION: &str = "...";
@@ -11,29 +13,41 @@ const DEPLOYMENT_ID: &str = "...";
 
 pub fn app(cx: Scope) -> Element {
     const GREETING: &str = "Hello! How are you?";
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(512u16)
+        .model("gpt-3.5-turbo-0613")
+        .messages([
+            ChatCompletionRequestMessage::User(
+                ChatCompletionRequestUserMessageArgs::default()
+                    .content(GREETING)
+                    .build()
+                    .unwrap()
+            )
+        ])
+        .build().unwrap();
     let response_string: &UseRef<String> = use_ref(cx, String::new);
     let fetch_completion_chunks: &Coroutine<()> = use_coroutine(cx, |rx| {
         let response_string = response_string.to_owned();
         async move {
-            let config = async_openai::config::AzureConfig::new()
-                .with_api_base(API_BASE)
-                .with_api_key(API_KEY)
-                .with_api_version(API_VERSION)
-                .with_deployment_id(DEPLOYMENT_ID);
-            let client = async_openai::Client::with_config(config);
-            let request = CreateChatCompletionRequestArgs::default()
-                .max_tokens(512u16)
-                .model("gpt-3.5-turbo-0613")
-                .messages([
-                    ChatCompletionRequestMessage::User(
-                        ChatCompletionRequestUserMessageArgs::default()
-                            .content(GREETING)
-                            .build()
-                            .unwrap()
-                    )
-                ])
-                .build().unwrap();
-            let mut stream = client.chat().create_stream(request).await.unwrap();
+            let mut stream = if USE_AZURE {
+                let config = async_openai::config::AzureConfig::new()
+                    .with_api_base(API_BASE)
+                    .with_api_key(API_KEY)
+                    .with_api_version(API_VERSION)
+                    .with_deployment_id(DEPLOYMENT_ID);
+                let client = async_openai::Client::with_config(config);
+                client.chat().create_stream(request).await.unwrap()
+            } else {
+                let config = async_openai::config::OpenAIConfig::new()
+                    .with_api_key(API_KEY);
+                let config = if API_BASE != "..." {
+                    config.with_api_base(API_BASE)
+                } else {
+                    config
+                };
+                let client = async_openai::Client::with_config(config);
+                client.chat().create_stream(request).await.unwrap()
+            };
             while let Some(chunk) = stream.next().await {
                 match chunk {
                     Ok(response) => {
@@ -57,6 +71,13 @@ pub fn app(cx: Scope) -> Element {
 
     render! {
         div {
+            p {
+                if USE_AZURE {
+                    "Using Azure OpenAI"
+                } else {
+                    "Using OpenAI"
+                }
+            }
             p {
                 "User: {GREETING}"
             }
