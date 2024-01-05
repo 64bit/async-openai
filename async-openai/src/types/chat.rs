2b964ed80e60fdd1f3b1bd660d086b79e4862d3a
Himanshu Neema <himanshun.iitkgp@gmail.com>
2024-01-04 21:25:56 -0800

    deprecated

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -292,19 +292,16 @@ pub struct ChatCompletionResponseMessage {
 #[builder(setter(into, strip_option), default)]
 #[builder(derive(Debug))]
 #[builder(build_fn(error = "OpenAIError"))]
+#[deprecated]
 pub struct ChatCompletionFunctions {
     /// The name of the function to be called. Must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64.
     pub name: String,
     /// A description of what the function does, used by the model to choose when and how to call the function.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    /// The parameters the functions accepts, described as a JSON Schema object.
-    /// See the [guide](https://platform.openai.com/docs/guides/gpt/function-calling) for examples,
-    /// and the [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for
-    /// documentation about the format.
+    /// The parameters the functions accepts, described as a JSON Schema object. See the [guide](https://platform.openai.com/docs/guides/text-generation/function-calling) for examples, and the [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for documentation about the format.
     ///
-    /// To describe a function that accepts no parameters, provide the
-    /// value `{\"type\": \"object\", \"properties\": {}}`.
+    /// Omitting `parameters` defines a function with an empty parameter list.
     pub parameters: serde_json::Value,
 }
 
