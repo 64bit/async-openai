c2e7ed30387cfcdb052613abefb56c00fa3151cb
Adrien <adrien@genei.io>
2024-02-08 01:44:51 -0800

    add support for base64 embeddings

--- async-openai/src/embedding.rs
+++ async-openai/src/embedding.rs
@@ -1,7 +1,11 @@
+use base64::engine::{general_purpose, Engine};
+
 use crate::{
     config::Config,
     error::OpenAIError,
-    types::{CreateEmbeddingRequest, CreateEmbeddingResponse},
+    types::{
+        Base64Embedding, CreateEmbeddingRequest, CreateEmbeddingResponse, Embedding, EncodingFormat,
+    },
     Client,
 };
 
@@ -23,13 +27,55 @@ impl<'c, C: Config> Embeddings<'c, C> {
         &self,
         request: CreateEmbeddingRequest,
     ) -> Result<CreateEmbeddingResponse, OpenAIError> {
-        self.client.post("/embeddings", request).await
+        if !matches!(request.encoding_format, Some(EncodingFormat::Base64)) {
+            self.client.post("/embeddings", request).await
+        } else {
+            let response: CreateEmbeddingResponse<Base64Embedding> =
+                self.client.post("/embeddings", request).await?;
+            response.try_into()
+        }
+    }
+}
+
+impl TryFrom<CreateEmbeddingResponse<Base64Embedding>> for CreateEmbeddingResponse<Embedding> {
+    type Error = OpenAIError;
+
+    fn try_from(response: CreateEmbeddingResponse<Base64Embedding>) -> Result<Self, Self::Error> {
+        let response = CreateEmbeddingResponse {
+            model: response.model,
+            object: response.object,
+            usage: response.usage,
+            data: response
+                .data
+                .into_iter()
+                .map(Embedding::try_from)
+                .collect::<Result<_, _>>()?,
+        };
+        Ok(response)
+    }
+}
+
+impl TryFrom<Base64Embedding> for Embedding {
+    type Error = OpenAIError;
+
+    fn try_from(embedding: Base64Embedding) -> Result<Self, Self::Error> {
+        let bytes = general_purpose::STANDARD.decode(embedding.embedding)?;
+        let chunks = bytes.chunks_exact(4);
+        debug_assert!(chunks.remainder().len() == 0);
+        let floats = chunks
+            .map(|chunk| f32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]))
+            .collect();
+        Ok(Embedding {
+            index: embedding.index,
+            object: embedding.object,
+            embedding: floats,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::types::{CreateEmbeddingResponse, Embedding};
+    use crate::types::{CreateEmbeddingResponse, Embedding, EncodingFormat};
     use crate::{types::CreateEmbeddingRequestArgs, Client};
 
     #[tokio::test]
@@ -126,5 +172,35 @@ mod tests {
         assert_eq!(data.len(), 1);
         let Embedding { embedding, .. } = data.pop().unwrap();
         assert_eq!(embedding.len(), dimensions as usize);
+    }
+
+    #[tokio::test]
+    async fn test_base64_embedding() {
+        let client = Client::new();
+
+        const MODEL: &str = "text-embedding-ada-002";
+        const INPUT: &str = "CoLoop will eat the other qual research tools...";
+
+        let b64_request = CreateEmbeddingRequestArgs::default()
+            .model(MODEL)
+            .input(INPUT)
+            .encoding_format(EncodingFormat::Base64)
+            .build()
+            .unwrap();
+        let b64_response = client.embeddings().create(b64_request).await.unwrap();
+        let b64_embedding = b64_response.data.into_iter().next().unwrap().embedding;
+
+        let request = CreateEmbeddingRequestArgs::default()
+            .model(MODEL)
+            .input(INPUT)
+            .build()
+            .unwrap();
+        let response = client.embeddings().create(request).await.unwrap();
+        let embedding = response.data.into_iter().next().unwrap().embedding;
+
+        assert_eq!(b64_embedding.len(), embedding.len());
+        for (b64, normal) in b64_embedding.iter().zip(embedding.iter()) {
+            assert!((b64 - normal).abs() < 1e-6);
+        }
     }
 }
