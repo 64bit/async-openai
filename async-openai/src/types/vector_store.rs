d0149513cbb6156dd425fec6c957359b87b7b878
Himanshu Neema <himanshun.iitkgp@gmail.com>
2024-05-06 22:37:40 -0700

    fix metadata type in vector_store.rs type

--- async-openai/src/types/vector_store.rs
+++ async-openai/src/types/vector_store.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
@@ -23,7 +25,7 @@ pub struct CreateVectorStoreRequest {
 
     /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maximum of 512 characters long.
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub metadata: Option<serde_json::Value>,
+    pub metadata: Option<HashMap<String, serde_json::Value>>,
 }
 
 /// Vector store expiration policy
@@ -58,7 +60,7 @@ pub struct VectorStoreObject {
     pub last_active_at: Option<u32>,
 
     /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maximum of 512 characters long.
-    pub metadata: Option<serde_json::Value>,
+    pub metadata: Option<HashMap<String, serde_json::Value>>,
 }
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
@@ -111,7 +113,7 @@ pub struct UpdateVectorStoreRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub expires_after: Option<VectorStoreExpirationAfter>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub metadata: Option<serde_json::Value>,
+    pub metadata: Option<HashMap<String, serde_json::Value>>,
 }
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
