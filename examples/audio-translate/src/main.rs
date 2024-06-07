09cee248d1be037dba5f871f5cae58749c9a170a
Himanshu Neema <himanshun.iitkgp@gmail.com>
2024-06-07 15:38:11 -0700

    udpated translation example to include SRT

--- examples/audio-translate/src/main.rs
+++ examples/audio-translate/src/main.rs
@@ -1,8 +1,25 @@
-use async_openai::{types::CreateTranslationRequestArgs, Client};
+use async_openai::{
+    types::{AudioResponseFormat, CreateTranslationRequestArgs},
+    Client,
+};
 use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn translate_srt() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+    let request = CreateTranslationRequestArgs::default()
+        .file("./audio/koshish karne walon ki haar nahi hoti by amitabh bachchan_320kbps.mp3")
+        .model("whisper-1")
+        .response_format(AudioResponseFormat::Srt)
+        .build()?;
+
+    let response = client.audio().translate_raw(request).await?;
+
+    println!("translate_srt:");
+    println!("{}", String::from_utf8_lossy(response.as_ref()));
+    Ok(())
+}
+
+async fn translate_verbose_json() -> Result<(), Box<dyn Error>> {
     let client = Client::new();
     // Credits and Source for audio: https://www.youtube.com/watch?v=bHWmzQ4HTS0
     let request = CreateTranslationRequestArgs::default()
@@ -12,7 +29,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let response = client.audio().translate(request).await?;
 
+    println!("translate_verbose_json:");
     println!("{}", response.text);
 
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    translate_verbose_json().await?;
+    translate_srt().await?;
     Ok(())
 }
