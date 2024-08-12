2ff445e2cfd95f6b6c425d174d3dd4ddad763323
Domen Kožar <domen@dev.si>
2024-08-12 22:17:30 +0100

    Support structured outputs

--- async-openai/src/types/assistant.rs
+++ async-openai/src/types/assistant.rs
@@ -129,8 +129,33 @@ pub enum AssistantsApiResponseFormatOption {
 /// An object describing the expected output of the model. If `json_object` only `function` type `tools` are allowed to be passed to the Run. If `text` the model can return text or any value needed.
 #[derive(Clone, Serialize, Debug, Deserialize, PartialEq, Default)]
 pub struct AssistantsApiResponseFormat {
-    /// Must be one of `text` or `json_object`.
+    /// Must be one of `text`, `json_object` or `json_schema`.
     pub r#type: AssistantsApiResponseFormatType,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub json_schema: Option<AssistantJsonSchemaObject>,
+}
+
+#[derive(Clone, Serialize, Default, Debug, Deserialize, Builder, PartialEq)]
+#[builder(name = "AssistantJsonSchemaObjectArgs")]
+#[builder(pattern = "mutable")]
+#[builder(setter(into, strip_option), default)]
+#[builder(derive(Debug))]
+#[builder(build_fn(error = "OpenAIError"))]
+pub struct AssistantJsonSchemaObject {
+    /// The name of the JSONSchema. Must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64.
+    pub name: String,
+    /// A description of what the function does, used by the model to choose when and how to call the function.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    /// The parameters the functions accepts, described as a JSON Schema object. See the [guide](https://platform.openai.com/docs/guides/text-generation/function-calling) for examples, and the [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for documentation about the format.
+    ///
+    /// Omitting `parameters` defines a function with an empty parameter list.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub schema: Option<serde_json::Value>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub strict: Option<bool>,
 }
 
 #[derive(Clone, Serialize, Debug, Deserialize, PartialEq, Default)]
@@ -139,6 +164,7 @@ pub enum AssistantsApiResponseFormatType {
     #[default]
     Text,
     JsonObject,
+    JsonSchema,
 }
 
 /// Retrieval tool
