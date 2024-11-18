9274131ed7d5f472e55ac4951914e5e70b089d69
Niklas Sarup-Lytzen <71767810+nikessel@users.noreply.github.com>
2024-11-18 05:50:13 +0100

    Fix chunking strategy serialization and field visibility (#288)
    * Fix AssistantVectorStoreChunkingStrategy rename and make StaticChunkingStrategy fields public
    
    * Fix VectorStoreChunkingStrategy rename
    
    * Fix chunking stategy static field name
    
    ---------
    
    Co-authored-by: Niklas <71767810+niklass-l@users.noreply.github.com>

--- async-openai/src/types/assistant.rs
+++ async-openai/src/types/assistant.rs
@@ -60,19 +60,24 @@ pub struct AssistantVectorStore {
 pub enum AssistantVectorStoreChunkingStrategy {
     /// The default strategy. This strategy currently uses a `max_chunk_size_tokens` of `800` and `chunk_overlap_tokens` of `400`.
     #[default]
+    #[serde(rename = "auto")]
     Auto,
-    Static(StaticChunkingStrategy),
+    #[serde(rename = "static")]
+    Static {
+        #[serde(rename = "static")]
+        config: StaticChunkingStrategy,
+    },
 }
 
 /// Static Chunking Strategy
 #[derive(Clone, Serialize, Debug, Deserialize, PartialEq, Default)]
 pub struct StaticChunkingStrategy {
     /// The maximum number of tokens in each chunk. The default value is `800`. The minimum value is `100` and the maximum value is `4096`.
-    max_chunk_size_tokens: u16,
+    pub max_chunk_size_tokens: u16,
     /// The number of tokens that overlap between chunks. The default value is `400`.
     ///
     /// Note that the overlap must not exceed half of `max_chunk_size_tokens`.
-    chunk_overlap_tokens: u16,
+    pub chunk_overlap_tokens: u16,
 }
 
 /// Represents an `assistant` that can call the model and use tools.
