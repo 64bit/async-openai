751280eddc0f16423e543ac20fa04f31a5538923
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-01-09 16:20:32 -0800

    updated AssistantObject

--- async-openai/src/types/assistant.rs
+++ async-openai/src/types/assistant.rs
@@ -93,20 +93,19 @@ pub struct AssistantObject {
     pub name: Option<String>,
     /// The description of the assistant. The maximum length is 512 characters.
     pub description: Option<String>,
+    /// ID of the model to use. You can use the [List models](https://platform.openai.com/docs/api-reference/models/list) API to see all of your available models, or see our [Model overview](https://platform.openai.com/docs/models) for descriptions of them.
     pub model: String,
     /// The system instructions that the assistant uses. The maximum length is 256,000 characters.
     pub instructions: Option<String>,
     /// A list of tool enabled on the assistant. There can be a maximum of 128 tools per assistant. Tools can be of types `code_interpreter`, `file_search`, or `function`.
+    #[serde(default)]
     pub tools: Vec<AssistantTools>,
-
     /// A set of resources that are used by the assistant's tools. The resources are specific to the type of tool. For example, the `code_interpreter` tool requires a list of file IDs, while the `file_search` tool requires a list of vector store IDs.
     pub tool_resources: Option<AssistantToolResources>,
-    /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maxium of 512 characters long.
-    pub metadata: Option<HashMap<String, serde_json::Value>>,
-
+    /// Set of 16 key-value pairs that can be attached to an object. This can be useful for storing additional information about the object in a structured format. Keys can be a maximum of 64 characters long and values can be a maximum of 512 characters long.
+    pub metadata: Option<HashMap<String, String>>,
     /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
     pub temperature: Option<f32>,
-
     /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
     ///
     /// We generally recommend altering this or temperature but not both.
