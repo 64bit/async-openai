67bdbcd0e994fee1642fb01d1013d3a70637ac13
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-01-09 16:52:22 -0800

    updated doc comment for ResponseFormatJsonSchema in types/chat.rs

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -416,7 +416,7 @@ pub struct ResponseFormatJsonSchema {
     /// A description of what the response format is for, used by the model to determine how to respond in the format.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    /// The name of the response format. Must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length
+    /// The name of the response format. Must be a-z, A-Z, 0-9, or contain underscores and dashes, with a maximum length of 64.
     pub name: String,
     /// The schema for the response format, described as a JSON Schema object.
     #[serde(skip_serializing_if = "Option::is_none")]
