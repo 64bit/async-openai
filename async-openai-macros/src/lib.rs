937cc542ce8e7b3cf30175e15fae1d7054a46240
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-03-02 14:15:45 -0800

    format

--- async-openai-macros/src/lib.rs
+++ async-openai-macros/src/lib.rs
@@ -5,24 +5,24 @@ use syn::{
     parse_macro_input,
     punctuated::Punctuated,
     token::Comma,
-    FnArg, GenericParam, Generics, ItemFn, Pat, PatType, ReturnType, Type, TypeParam, TypeParamBound,
-    WhereClause,
+    FnArg, GenericParam, Generics, ItemFn, Pat, PatType, ReturnType, Type, TypeParam,
+    TypeParamBound, WhereClause,
 };
 
 // Parse attribute arguments like #[byot(T0: Display + Debug, T1: Clone, R: Serialize)]
 struct BoundArgs {
     bounds: Vec<(String, syn::TypeParamBound)>,
     where_clause: Option<String>,
-    stream: bool,  // Add stream flag
+    stream: bool, // Add stream flag
 }
 
 impl Parse for BoundArgs {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut bounds = Vec::new();
         let mut where_clause = None;
-        let mut stream = false;  // Default to false
+        let mut stream = false; // Default to false
         let vars = Punctuated::<syn::MetaNameValue, Comma>::parse_terminated(input)?;
-        
+
         for var in vars {
             let name = var.path.get_ident().unwrap().to_string();
             match name.as_str() {
@@ -33,12 +33,17 @@ impl Parse for BoundArgs {
                     stream = var.value.into_token_stream().to_string().contains("true");
                 }
                 _ => {
-                    let bound: syn::TypeParamBound = syn::parse_str(&var.value.into_token_stream().to_string())?;
+                    let bound: syn::TypeParamBound =
+                        syn::parse_str(&var.value.into_token_stream().to_string())?;
                     bounds.push((name, bound));
                 }
             }
         }
-        Ok(BoundArgs { bounds, where_clause, stream })
+        Ok(BoundArgs {
+            bounds,
+            where_clause,
+            stream,
+        })
     }
 }
 
@@ -56,29 +61,39 @@ pub fn byot(args: TokenStream, item: TokenStream) -> TokenStream {
 
     // Process function arguments
     let mut new_params = Vec::new();
-    let args = input.sig.inputs.iter().map(|arg| {
-        match arg {
-            FnArg::Receiver(receiver) => receiver.to_token_stream(),
-            FnArg::Typed(PatType { pat, ty, .. }) => {
-                if let Pat::Ident(pat_ident) = &**pat {
-                    let generic_name = format!("T{}", param_count);
-                    let generic_ident = syn::Ident::new(&generic_name, proc_macro2::Span::call_site());
-                    
-                    // Create type parameter with optional bounds
-                    let mut type_param = TypeParam::from(generic_ident.clone());
-                    if let Some((_, bound)) = bounds_args.bounds.iter().find(|(name, _)| name == &generic_name) {
-                        type_param.bounds.extend(vec![bound.clone()]);
+    let args = input
+        .sig
+        .inputs
+        .iter()
+        .map(|arg| {
+            match arg {
+                FnArg::Receiver(receiver) => receiver.to_token_stream(),
+                FnArg::Typed(PatType { pat, ty, .. }) => {
+                    if let Pat::Ident(pat_ident) = &**pat {
+                        let generic_name = format!("T{}", param_count);
+                        let generic_ident =
+                            syn::Ident::new(&generic_name, proc_macro2::Span::call_site());
+
+                        // Create type parameter with optional bounds
+                        let mut type_param = TypeParam::from(generic_ident.clone());
+                        if let Some((_, bound)) = bounds_args
+                            .bounds
+                            .iter()
+                            .find(|(name, _)| name == &generic_name)
+                        {
+                            type_param.bounds.extend(vec![bound.clone()]);
+                        }
+
+                        new_params.push(GenericParam::Type(type_param));
+                        param_count += 1;
+                        quote! { #pat_ident: #generic_ident }
+                    } else {
+                        arg.to_token_stream()
                     }
-                    
-                    new_params.push(GenericParam::Type(type_param));
-                    param_count += 1;
-                    quote! { #pat_ident: #generic_ident }
-                } else {
-                    arg.to_token_stream()
                 }
             }
-        }
-    }).collect::<Vec<_>>();
+        })
+        .collect::<Vec<_>>();
 
     // Add R type parameter with optional bounds
     let generic_r = syn::Ident::new("R", proc_macro2::Span::call_site());
@@ -102,9 +117,7 @@ pub fn byot(args: TokenStream, item: TokenStream) -> TokenStream {
     let where_clause = if let Some(where_str) = bounds_args.where_clause {
         match syn::parse_str::<WhereClause>(&format!("where {}", where_str.replace("\"", ""))) {
             Ok(where_clause) => quote! { #where_clause },
-            Err(e) => {
-                return TokenStream::from(e.to_compile_error())
-            }
+            Err(e) => return TokenStream::from(e.to_compile_error()),
         }
     } else {
         quote! {}
