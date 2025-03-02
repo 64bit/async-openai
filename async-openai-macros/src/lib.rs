cf07e81bc9bb116a97d831ff04efa14979c381b3
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-03-01 21:12:43 -0800

    update macro

--- async-openai-macros/src/lib.rs
+++ async-openai-macros/src/lib.rs
@@ -1,39 +1,35 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, FnArg, GenericParam, Generics, ItemFn, Pat, PatType, ReturnType, Type};
+use syn::{
+    parse_macro_input, FnArg, GenericParam, Generics, ItemFn, Pat, PatType, ReturnType, Type,
+};
 
 #[proc_macro_attribute]
-pub fn byot(args: TokenStream, item: TokenStream) -> TokenStream {
-    // Parse the input function
+pub fn byot_passthrough(_args: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[proc_macro_attribute]
+pub fn byot(_args: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
-    
-    // Create new generic parameters for the function
     let mut new_generics = Generics::default();
-    let mut generic_mappings = Vec::new();
     let mut param_count = 0;
 
     // Process function arguments
     let mut new_params = Vec::new();
     let args = input.sig.inputs.iter().map(|arg| {
         match arg {
-            // Preserve self arguments as-is
-            FnArg::Receiver(receiver) => {
-                receiver.to_token_stream()
-            },
+            FnArg::Receiver(receiver) => receiver.to_token_stream(),
             FnArg::Typed(PatType { pat, ty, .. }) => {
                 if let Pat::Ident(pat_ident) = &**pat {
-                    // Create a new generic parameter for this argument
                     let generic_ident = syn::Ident::new(
                         &format!("T{}", param_count),
-                        proc_macro2::Span::call_site()
+                        proc_macro2::Span::call_site(),
                     );
                     new_params.push(GenericParam::Type(
                         syn::TypeParam::from(generic_ident.clone())
                     ));
-                    generic_mappings.push((ty.clone(), generic_ident.clone()));
                     param_count += 1;
-
-                    // Return the argument with the new generic type
                     quote! { #pat_ident: #generic_ident }
                 } else {
                     arg.to_token_stream()
@@ -42,39 +38,30 @@ pub fn byot(args: TokenStream, item: TokenStream) -> TokenStream {
         }
     }).collect::<Vec<_>>();
 
-    // Handle return type
-    let (return_generic, return_param) = if let ReturnType::Type(arrow, _) = &input.sig.output {
-        let generic_ident = syn::Ident::new("R", proc_macro2::Span::call_site());
-        let param = GenericParam::Type(syn::TypeParam::from(generic_ident.clone()));
-        (quote! { #arrow #generic_ident }, Some(param))
-    } else {
-        (input.sig.output.to_token_stream(), None)
-    };
+    // Add R type parameter for return type
+    let generic_r = syn::Ident::new("R", proc_macro2::Span::call_site());
+    let return_param = GenericParam::Type(syn::TypeParam::from(generic_r.clone()));
+    new_params.push(return_param);
 
-    // Add all generic parameters at once
+    // Add all generic parameters
     new_generics.params.extend(new_params);
-    if let Some(param) = return_param {
-        new_generics.params.push(param);
-    }
 
-    // Generate the new function
+    // Generate the new function with Result<R, OpenAIError> return type
     let fn_name = &input.sig.ident;
-    let byot_fn_name = syn::Ident::new(
-        &format!("{}_byot", fn_name),
-        fn_name.span()
-    );
+    let byot_fn_name = syn::Ident::new(&format!("{}_byot", fn_name), fn_name.span());
     let vis = &input.vis;
     let block = &input.block;
     let attrs = &input.attrs;
-    
+    let asyncness = &input.sig.asyncness;
+
     let expanded = quote! {
         // Original function
         #(#attrs)*
-        #vis #input
+        #input
 
         // Generated generic function with _byot suffix
         #(#attrs)*
-        #vis fn #byot_fn_name #new_generics (#(#args),*) #return_generic #block
+        #vis #asyncness fn #byot_fn_name #new_generics (#(#args),*) -> Result<R, OpenAIError> #block
     };
 
     expanded.into()
