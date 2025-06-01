939c4cdcef23fe8bda786b3e28ac0deee283bb06
Eric Christiansen <ericmartinchristiansen@gmail.com>
2025-06-01 16:36:29 -0700

    Add instructions option to speech request (#374)
    * Add instructions field to speech request
    
    * Update async-openai/src/types/audio.rs
    
    * Update openapi.yaml
    
    ---------
    
    Co-authored-by: Himanshu Neema <himanshun.iitkgp@gmail.com>

--- async-openai/src/types/audio.rs
+++ async-openai/src/types/audio.rs
@@ -188,9 +188,15 @@ pub struct CreateSpeechRequest {
     /// One of the available [TTS models](https://platform.openai.com/docs/models/tts): `tts-1` or `tts-1-hd`
     pub model: SpeechModel,
 
-    /// The voice to use when generating the audio. Supported voices are `alloy`, `ash`, `coral`, `echo`, `fable`, `onyx`, `nova`, `sage` and `shimmer`.
+    /// The voice to use when generating the audio. Supported voices are `alloy`, `ash`, `coral`, `echo`, `fable`, `onyx`, `nova`, `sage`, `shimmer` and `verse`.
+
     /// Previews of the voices are available in the [Text to speech guide](https://platform.openai.com/docs/guides/text-to-speech#voice-options).
     pub voice: Voice,
+
+    /// Control the voice of your generated audio with additional instructions.
+    /// Does not work with `tts-1` or `tts-1-hd`.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub instructions: Option<String>,
 
     /// The format to audio in. Supported formats are `mp3`, `opus`, `aac`, `flac`, `wav`, and `pcm`.
     #[serde(skip_serializing_if = "Option::is_none")]
