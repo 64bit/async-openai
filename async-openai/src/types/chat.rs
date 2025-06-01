207aeb7edff03873ada6151564b976e692ce62bf
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-06-01 16:09:54 -0700

    Update async-openai/src/types/chat.rs

--- async-openai/src/types/chat.rs
+++ async-openai/src/types/chat.rs
@@ -572,14 +572,19 @@ pub enum WebSearchUserLocationType {
     Approximate,
 }
 
+/// Approximate location parameters for the search.
 #[derive(Clone, Serialize, Debug, Default, Deserialize, PartialEq)]
 pub struct WebSearchLocation {
-
-    pub city: Option<String>,
+    ///  The two-letter [ISO country code](https://en.wikipedia.org/wiki/ISO_3166-1) of the user, e.g. `US`.
     pub country: Option<String>,
+    /// Free text input for the region of the user, e.g. `California`.
     pub region: Option<String>,
+    /// Free text input for the city of the user, e.g. `San Francisco`.
+    pub city: Option<String>,
+    /// The [IANA timezone](https://timeapi.io/documentation/iana-timezones) of the user, e.g. `America/Los_Angeles`.
     pub timezone: Option<String>,
 }
+
 
 #[derive(Clone, Serialize, Debug, Deserialize, PartialEq)]
 pub struct WebSearchUserLocation {
