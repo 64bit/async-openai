26542d05d387592a4e643fde565498d80735e08b
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-11-01 13:13:30 -0700

    updated rate limit

--- async-openai/src/types/realtime/rate_limit.rs
+++ async-openai/src/types/realtime/rate_limit.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RateLimit {
-    /// The name of the rate limit ("requests", "tokens", "input_tokens", "output_tokens").
+    /// The name of the rate limit (requests, tokens).
     pub name: String,
     /// The maximum allowed value for the rate limit.
     pub limit: u32,
