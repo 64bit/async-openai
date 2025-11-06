3bb7eb7d9d25d3c379f43953a7e1ac1f6ed726e6
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-11-05 17:50:24 -0800

    feat: containers api

--- async-openai/src/client.rs
+++ async-openai/src/client.rs
@@ -13,7 +13,7 @@ use crate::{
     image::Images,
     moderation::Moderations,
     traits::AsyncTryFrom,
-    Assistants, Audio, AuditLogs, Batches, Chat, Completions, Conversations, Embeddings,
+    Assistants, Audio, AuditLogs, Batches, Chat, Completions, Containers, Conversations, Embeddings,
     FineTuning, Invites, Models, Projects, Responses, Threads, Uploads, Users, VectorStores,
     Videos,
 };
@@ -176,6 +176,11 @@ impl<C: Config> Client<C> {
     /// To call [Conversations] group related APIs using this client.
     pub fn conversations(&self) -> Conversations<'_, C> {
         Conversations::new(self)
+    }
+
+    /// To call [Containers] group related APIs using this client.
+    pub fn containers(&self) -> Containers<'_, C> {
+        Containers::new(self)
     }
 
     pub fn config(&self) -> &C {
