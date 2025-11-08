cf89e73ed56acfce71f1c69c7a981809fdfff835
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-11-08 12:20:07 -0800

    update image edit

--- examples/create-image-edit/src/main.rs
+++ examples/create-image-edit/src/main.rs
@@ -1,9 +1,8 @@
 use async_openai::{
-    types::images::{CreateImageEditRequestArgs, DallE2ImageSize, ImageResponseFormat},
+    types::images::{CreateImageEditRequestArgs, ImageResponseFormat, ImageSize},
     Client,
 };
 use std::error::Error;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::new();
@@ -13,12 +12,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .mask("./images/mask.png")
         .prompt("A sunlit indoor lounge area with a duck in the pool")
         .n(1)
-        .size(DallE2ImageSize::S1024x1024)
+        .size(ImageSize::S1024x1024)
         .response_format(ImageResponseFormat::Url)
         .user("async-openai")
         .build()?;
 
-    let response = client.images().create_edit(request).await?;
+    let response = client.images().edit(request).await?;
 
     let paths = response.save("./data").await?;
 
