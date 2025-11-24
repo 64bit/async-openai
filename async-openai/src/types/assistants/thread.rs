833b113ca8a19c2be7b6329edf851ced5aa6d693
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-11-24 12:18:26 -0800

    consistent imports

--- async-openai/src/types/assistants/thread.rs
+++ async-openai/src/types/assistants/thread.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::error::OpenAIError;
 
-use super::{
+use crate::types::assistants::{
     AssistantToolResources, AssistantTools, AssistantsApiResponseFormatOption,
     AssistantsApiToolChoiceOption, CreateAssistantToolResources, CreateMessageRequest,
     TruncationObject,
