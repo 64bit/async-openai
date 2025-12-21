5f81a1c68f9f53bd870b313e18762a552e0235d6
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-12-20 16:52:55 -0800

    types/input_source.rs for wasm

--- async-openai/src/types/input_source.rs
+++ async-openai/src/types/input_source.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, Clone, PartialEq)]
 pub enum InputSource {
+    #[cfg(not(target_family = "wasm"))]
     Path {
         path: std::path::PathBuf,
     },
@@ -13,10 +14,21 @@ pub enum InputSource {
     },
 }
 
+#[cfg(not(target_family = "wasm"))]
 impl Default for InputSource {
     fn default() -> Self {
         InputSource::Path {
             path: std::path::PathBuf::new(),
+        }
+    }
+}
+
+#[cfg(target_family = "wasm")]
+impl Default for InputSource {
+    fn default() -> Self {
+        InputSource::Bytes {
+            filename: String::new(),
+            bytes: bytes::Bytes::new(),
         }
     }
 }
