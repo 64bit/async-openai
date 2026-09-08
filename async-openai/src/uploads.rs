1144b48877eb487d9ac3e32e9f5e92ffd554b572
Himanshu Neema <himanshun.iitkgp@gmail.com>
2026-09-07 23:44:41 -0700

    uploads.rs doc comment

--- async-openai/src/uploads.rs
+++ async-openai/src/uploads.rs
@@ -28,9 +28,8 @@ impl<'c, C: Config> Uploads<'c, C> {
     /// Once you complete the Upload, we will create a [File](https://platform.openai.com/docs/api-reference/files/object)
     /// object that contains all the parts you uploaded. This File is usable in the rest of our platform as a regular File object.
     ///            
-    /// For certain `purpose`s, the correct `mime_type` must be specified. Please refer to documentation for the
-    /// supported MIME types for your use case:
-    /// - [Assistants](https://platform.openai.com/docs/assistants/tools/file-search/supported-files)
+    /// For certain `purpose` values, the correct `mime_type` must be specified. Please refer to documentation for the
+    /// [supported MIME types for your use case](https://platform.openai.com/docs/guides/tools-file-search#supported-files)
     ///
     /// For guidance on the proper filename extensions for each purpose, please follow the documentation on
     /// [creating a File](https://platform.openai.com/docs/api-reference/files/create).
