d4f5b792324ae1a7934193220ce1b5d0a27ff2f4
Himanshu Neema <himanshun.iitkgp@gmail.com>
2026-09-08 00:00:26 -0700

    update to async-openai/src/types/models/model.rs

--- async-openai/src/types/models/model.rs
+++ async-openai/src/types/models/model.rs
@@ -11,6 +11,9 @@ pub struct Model {
     pub created: u32,
     /// The organization that owns the model.
     pub owned_by: String,
+    /// The date when the model will shut down, or null if not announced.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub shutdown_date: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
